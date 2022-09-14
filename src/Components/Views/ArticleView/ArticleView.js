d989ae5aeec940ebc1544d918e5306859419ea85
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-09-14 11:15:23 -0600

    Add initial view to article view

--- src/Components/Views/ArticleView/ArticleView.js
+++ src/Components/Views/ArticleView/ArticleView.js
@@ -1,7 +1,14 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useState } from "react"
 
 const ArticleView = ({articles, id}) => {
+  const [selectedArticle, setSelectedArticle] = useState('')
   console.log(id)
+
+  useEffect(() => {
+    articles.length && setSelectedArticle(articles.find(article => article.title === id))
+  }, [])
+  
   return (
     <div className="title">Article</div>
   )
