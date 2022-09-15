45bcae27dcedd5e5d33124e14e2eaf2b943f8719
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-09-15 10:37:49 -0600

    Add .env to hide key

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 export const getArticles = (section) => {
-  return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=KpGXEIs5cAOuYVP8yMXUB6KozjLLEkzC`)
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${process.env.REACT_APP_API_KEY}`)
     .then(response => {
       if (response.ok) {
         return response.json()
